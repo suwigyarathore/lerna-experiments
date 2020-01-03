0f2b3f5be9341cc916ab8195237c57cb3d8bc2c8
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2020-01-04 00:23:04 +0100

    Initia commit with hoisting

---
+++ packages/grocery/index.js
@@ -0,0 +1,6 @@
+const sillyname = require("sillyname");
+const apple = require("apple");
+const banana = require("banana");
+console.log(`grocery and ${sillyname()}`);
+console.log(apple);
+console.log(banana);
