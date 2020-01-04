acf602213e20b48005e2cecc9856376a7591c777
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2020-01-04 02:17:02 +0100

    Setting up testing

---
+++ packages/apple/index.test.js
@@ -0,0 +1,4 @@
+const apple = require("./");
+test("returns correct string", () => {
+  expect(apple).toBe("apple and TEST");
+});
---
+++ packages/banana/index.test.js
@@ -0,0 +1,4 @@
+const banana = require("./");
+test("returns correct string", () => {
+  expect(banana).toBe("banana and TEST");
+});
